71a7af09726835dff43d37241f6fd2cf7788267d
Douglas Thomaz <douglas.thomaz@spot-edu.com.br>
2020-07-01 19:39:24 -0300

    RO-14 adding list to admin

--- src/pages/AdminPanel/CreateMemberForm.tsx
+++ src/pages/AdminPanel/CreateMemberForm.tsx
@@ -3,8 +3,15 @@ import * as Yup from 'yup';
 import { Form } from '@unform/web';
 import { useHistory } from 'react-router-dom';
 import { FormHandles } from '@unform/core';
-import { FiUser, FiMail } from 'react-icons/fi';
-import { FormContainer } from './styles';
+import { FiUser, FiMail, FiDelete, FiXCircle } from 'react-icons/fi';
+import {
+  List,
+  ListItem,
+  ListItemText,
+  ListItemIcon,
+  Divider,
+} from '@material-ui/core';
+import { FormContainer, Title } from './styles';
 import { useToast } from '../../hooks/toast';
 import api from '../../services/api';
 import getValidationErrors from '../../utils/getValidationErrors';
@@ -87,14 +94,45 @@ const CreateMemberForm: React.FC = () => {
       label: singleRole.role,
     }));
 
+  const mockedMembers = [
+    { id: 1, full_name: 'Douglas Thomaz' },
+    { id: 2, full_name: 'Fulano da Silva' },
+    { id: 3, full_name: 'Lorem Ipśum' },
+    { id: 4, full_name: 'Douglas Thomaz' },
+    { id: 5, full_name: 'Fulano da Silva' },
+    { id: 6, full_name: 'Lorem Ipśum' },
+    { id: 7, full_name: 'Douglas Thomaz' },
+    { id: 8, full_name: 'Fulano da Silva' },
+    { id: 9, full_name: 'Lorem Ipśum' },
+    { id: 10, full_name: 'Douglas Thomaz' },
+    { id: 11, full_name: 'Fulano da Silva' },
+    { id: 12, full_name: 'Lorem Ipśum' },
+  ];
+
   return (
     <FormContainer>
       <Form ref={formRef} onSubmit={handleSubmit}>
-        <Input name="name" icon={FiUser} placeholder="Nome" />
-        <Input name="email" icon={FiMail} placeholder="E-mail" />
+        <Title>Cadastre um novo membro</Title>
+        <Input name="name" icon={FiUser} placeholder="Nome Completo" />
+        <Input name="email" icon={FiMail} placeholder="E-mail Corp" />
         <Select name="role_id" options={options} placeholder="Função" />
         <Button type="submit">Cadastrar membro</Button>
       </Form>
+      <Divider />
+      <Title>Lista de membros:</Title>
+      <List aria-label="members show">
+        {mockedMembers.map((member) => (
+          <>
+            <Divider />
+            <ListItem key={member.id} button>
+              <ListItemText primary={member.full_name} />
+              <ListItemIcon>
+                <FiXCircle color="red" size={24} />
+              </ListItemIcon>
+            </ListItem>
+          </>
+        ))}
+      </List>
     </FormContainer>
   );
 };
--- src/pages/AdminPanel/styles.ts
+++ src/pages/AdminPanel/styles.ts
@@ -1,36 +1,25 @@
 import styled from 'styled-components';
 import { shade } from 'polished';
+import { List } from '@material-ui/core';
 
 export const Container = styled.div`
-  height: 100vh;
   display: flex;
   flex-direction: column;
-  justify-content: center;
+  justify-content: flex-start;
   align-items: center;
-`;
-
-export const FormContainer = styled.div`
-  display: flex !important;
-  height: 100% !important;
-  form {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-  }
 `;
 
 export const AdminContent = styled.div`
   max-width: 1000px;
-  height: 80%;
+  min-height: 500px;
   display: flex;
+  margin: 100px auto 25px auto;
   flex-direction: column;
   box-shadow: 2px 2px 5px #00000029;
   border-radius: 8px;
   background-color: ${shade(0.01, '#f8f8ff')};
   align-items: center;
   color: #019dda;
-  margin-top: 25px;
 
   button.MuiTab-textColorPrimary {
     span {
@@ -47,4 +36,32 @@ export const AdminContent = styled.div`
   span.PrivateTabIndicator-colorPrimary-2 {
     background-color: #019dda !important;
   }
+`;
+
+export const FormContainer = styled.div`
+  width: 100% !important;
+  form {
+    display: flex;
+    max-width: 500px;
+    margin: 25px auto;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+
+  ul {
+    display: flex;
+    flex-direction: column;
+    align-items: space-around;
+    max-width: 500px;
+    margin: 0 auto 40px auto;
+  }
+`;
+
+export const Title = styled.h1`
+  font-size: 24px;
+  font-weight: 500;
+  color: #019dda;
+  margin: 25px 0;
+  text-align: center;
 `;
